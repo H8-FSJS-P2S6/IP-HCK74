50e63a099ec31eaecb9cf57e2c4b5f416d029cae
Kevin <kevinpinn11@gmail.com>
2024-09-06 13:07:32 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const errorHandler = require("./middlewares/errorHandler");
 const router = require("./routers");
